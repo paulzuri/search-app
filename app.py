3c71b19397f56241897d610c58d0cba66367023a
Paul Zurita <paulzuri@hotmail.com>
2025-05-25 14:36:04 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Buscador de documentos")
+
+st.header('Selecciona tus documentos', divider='rainbow')
+
+st.markdown("Grupo 7")
+st.markdown("Soria, C., Zurita, M.")
+st.markdown("Modelo algebraico")
